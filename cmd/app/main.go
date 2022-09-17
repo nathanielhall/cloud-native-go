53db5d0dde72fe51d2f7ea8626017126dab05795
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-09-17 05:20:24 -0400

    initial commit

---
+++ cmd/app/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+ 
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Hello)	
+
+	log.Println("Starting server :8080")
+	
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal("Server startup failed")
+	}
+}
+ 
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+
+
+
+
+
+// log.Println("Starting server :8080")
+// s := &http.Server{
+// 	Addr:         ":8080",
+// 	Handler:      mux,
+// 	ReadTimeout:  30 * time.Second,
+// 	WriteTimeout: 30 * time.Second,
+// 	IdleTimeout:  120 * time.Second,
+// }
+// if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+// 	log.Fatal("Server startup failed")
+// }
+// }
+// func Greet(w http.ResponseWriter, r *http.Request) {
+// fmt.Fprintf(w, "Hello World!")
+// }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nathanielhall/cloud-native-go
+
+go 1.18
